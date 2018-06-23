490fadb526c208aa942c78ccf701360fe2a01af3
Saulo Carvalho <saulo.carv@hotmail.com>
2018-06-23 14:16:51 -0300

    Criação da classe pessoa e seus atributos

---
+++ Agenda/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataDeAniversario { get; set; }
+    }
+}
